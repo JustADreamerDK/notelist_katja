46a94094e4b93ddf054932c8d771b8572a159204
Katja Kjer <Kalle3008@hotmail.com>
2018-03-19 09:53:30 +0100

    push

--- javascript/script.js
+++ javascript/script.js
@@ -38,25 +38,43 @@ function buildList(){
     for(var i = 0; i < noteList.length; i++){
         var liElm = document.createElement("li");
         var pElm = document.createElement("p");
+        var divElm = document.createElement("div");
         var delBtnElm = document.createElement("button");
         var edtBtnElm = document.createElement("button");
+        var edtTimeBtnElm = document.createElement("button");
+
+        divElm.setAttribute("class", "buttons");
+        delBtnElm.setAttribute("class", "delete");
+        edtBtnElm.setAttribute("class", "edit");
+        edtTimeBtnElm.setAttribute("class", "edit-time");
 
         var date = new Date(noteList[i].dueDate);
 
-        pElm.innerHTML = noteList[i].text + " - " + ('0' + date.getDate()).slice(-2) + "/" + ('0' + (date.getMonth()+1)).slice(-2) + "-" + ('000' + date.getFullYear()).slice(-4) + " kl " + ('0' + date.getHours()).slice(-2) + ":" + ('0' + date.getMinutes()).slice(-2);
 
-        delBtnElm.innerHTML = "Delete";
-        delBtnElm.setAttribute("data-index", i);
+        if (noteList[i].important == true){
+            liElm.setAttribute("class", "important");
+        }
 
-        edtBtnElm.innerHTML = "Edit";
+        pElm.innerHTML = "<h3>" + noteList[i].text + "</h3><div class='time'>" + ('0' + date.getDate()).slice(-2) + "/" + ('0' + (date.getMonth()+1)).slice(-2) + "-" + ('000' + date.getFullYear()).slice(-4) + " kl " + ('0' + date.getHours()).slice(-2) + ":" + ('0' + date.getMinutes()).slice(-2) + "</div>";
+
+        edtBtnElm.innerHTML = "Edit note";
         edtBtnElm.setAttribute("data-index", i);
 
-        delBtnElm.addEventListener("click", submitDelEvent);
+        edtTimeBtnElm.innerHTML = "Edit time";
+        edtTimeBtnElm.setAttribute("data-index", i);
+
+        delBtnElm.innerHTML = "Delete note";
+        delBtnElm.setAttribute("data-index", i);
+
         edtBtnElm.addEventListener("click", submitEdtEvent);
+        edtTimeBtnElm.addEventListener("click", submitEdtDateEvent);
+        delBtnElm.addEventListener("click", submitDelEvent);
 
         liElm.appendChild(pElm);
-        liElm.appendChild(delBtnElm);
-        liElm.appendChild(edtBtnElm);
+        liElm.appendChild(divElm);
+        divElm.appendChild(edtBtnElm);
+        divElm.appendChild(edtTimeBtnElm);
+        divElm.appendChild(delBtnElm);
 
         ulElm.appendChild(liElm);
     }
@@ -78,6 +96,31 @@ function submitEdtEvent(event) {
     var newText = prompt("Hvad skal teksten være?");
 
     notes[arrIndex].text = newText;
+
+    setLocal(notes);
+    buildList();
+}
+
+function submitEdtDateEvent(event) {
+    var arrIndex = event.target.getAttribute("data-index");
+    var notes = getLocal();
+
+    var date = new Date(notes[arrIndex].dueDate);
+    var newDate = prompt("Nuværende dato " + ('0' + date.getDate()).slice(-2) + "/" + ('0' + (date.getMonth()+1)).slice(-2) + "-" + ('000' + date.getFullYear()).slice(-4) + " & nuværende tid: " + ('0' + date.getHours()).slice(-2) + ":" + ('0' + date.getMinutes()).slice(-2));
+
+    var aar = newDate.slice(-10, -6);
+    var maaned = newDate.slice(-13,-11);
+    var dag = newDate.slice(-16,-14);
+    var tid = newDate.slice(-2);
+    var minut = newDate.slice(-5,-3);
+
+    date.setDate(dag);
+    date.setMonth(maaned);
+    date.setFullYear(aar);
+    date.setHours(tid);
+    date.setMinutes(minut);
+
+    notes[arrIndex].dueDate = newDate;
 
     setLocal(notes);
     buildList();
